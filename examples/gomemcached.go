2b9bddf1ad259131cfb01c3cb8cfae70f7714802
loopthrough <ivan@loopthrough.ch>
2017-06-09 23:40:19 +0200

    Added simple example

---
+++ examples/gomemcached.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/bradfitz/gomemcache/memcache"
+	"github.com/loopthrough/mc-consistent-selector/selector"
+	"log"
+	"math/rand"
+	"strconv"
+)
+
+func main() {
+	sl := selector.NewServerList()
+	sl.Add("0.0.0.0:11211")
+	sl.Add("0.0.0.0:11212")
+	sl.Add("0.0.0.0:11213")
+
+	cache := memcache.NewFromSelector(sl)
+
+	err := cache.Set(&memcache.Item{
+		Key:   "test",
+		Value: []byte("test"),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = cache.Delete("test")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := 0; i < 1000; i++ {
+		key := rand.Int()
+		cache.Set(&memcache.Item{
+			Key:   strconv.Itoa(key),
+			Value: []byte(strconv.Itoa(key)),
+		})
+	}
+}
