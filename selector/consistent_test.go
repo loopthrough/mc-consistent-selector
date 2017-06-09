04ffae2665d10e286167bc5f0c2ccd49788f32fc
loopthrough <ivan@loopthrough.ch>
2017-06-09 22:56:04 +0200

    Tested distribution across nodes

--- selector/consistent_test.go
+++ selector/consistent_test.go
@@ -2,6 +2,9 @@ package selector
 
 import (
 	"github.com/stretchr/testify/assert"
+	"math/rand"
+	"net"
+	"strconv"
 	"testing"
 )
 
@@ -30,4 +33,32 @@ func TestPickServer1Server(t *testing.T) {
 	picked, _ := ch.PickForKey("any key")
 
 	assert.EqualValues(t, server, picked)
+}
+
+func TestDistributeAccrossServers(t *testing.T) {
+	server1 := &addrValue{"tcp", "127.0.0.1:11211"}
+	server2 := &addrValue{"tcp", "127.0.0.2:11211"}
+	server3 := &addrValue{"tcp", "127.0.0.3:11211"}
+	ch := New()
+	ch.Add(server1)
+	ch.Add(server2)
+	ch.Add(server3)
+
+	picks := map[net.Addr]int{
+		server1: 0,
+		server2: 0,
+		server3: 0,
+	}
+
+	for i := 0; i < 1000; i++ {
+		picked, _ := ch.PickForKey(strconv.Itoa(rand.Int()))
+		picks[picked] += 1
+	}
+
+	server1Picks, _ := picks[server1]
+	server2Picks, _ := picks[server2]
+	server3Picks, _ := picks[server2]
+	assert.InDelta(t, 300, server1Picks, 100)
+	assert.InDelta(t, 300, server2Picks, 100)
+	assert.InDelta(t, 300, server3Picks, 100)
 }
